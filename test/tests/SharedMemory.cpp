7ed57b498ebe4a53c5ccf99b36258eb313a1c80c
Vance Miller <vance@cs.utexas.edu>
2018-07-16 12:42:38 -0500

    update shared memory tests

--- test/tests/SharedMemory.cpp
+++ test/tests/SharedMemory.cpp
@@ -10,26 +10,26 @@ TEST(SharedMemory, CreateDestroyAnonymous) {
 }
 
 TEST(SharedMemory, CreateDestroyNamed) {
-  SharedMemory<int> sharedInt("intTest-1", O_RDWR, true, 0);
+  SharedMemory<int> sharedInt("intTest-1", O_RDWR, true, false);
 }
 
 TEST(SharedMemory, ReadWrite) {
-  SharedMemory<int> my_int("intTest0", O_RDWR, true, 0);
+  SharedMemory<int> my_int("intTest0", O_RDWR, true, false);
   int i = 0;
   for (*my_int = 0; *my_int < ITERATIONS; i++, (*my_int)++)
     EXPECT_EQ(*my_int, i);
 }
 
 TEST(SharedMemory, ReadOnly) {
-  SharedMemory<int> my_int("intTest1", O_RDWR, true, 0);
-  SharedMemory<int> your_int("intTest1", O_RDONLY, false, 0);
+  SharedMemory<int> my_int("intTest1", O_RDWR, true, false);
+  SharedMemory<int> your_int("intTest1", O_RDONLY, false, false);
   *my_int = 666;
   EXPECT_EQ(*my_int, *your_int);
 }
 
 TEST(SharedMemory, ReadWrite2) {
-  SharedMemory<int> my_int("intTest2", O_RDWR, true, 0);
-  SharedMemory<int> our_int("intTest2", O_RDWR, false, 0);
+  SharedMemory<int> my_int("intTest2", O_RDWR, true, false);
+  SharedMemory<int> our_int("intTest2", O_RDWR, false, false);
   *my_int = 666;
   EXPECT_EQ(*my_int, *our_int);
   (*our_int)++;
@@ -39,8 +39,8 @@ TEST(SharedMemory, ReadWrite2) {
 
 TEST(SharedMemory, Remap) {
   for (int i = 0; i < ITERATIONS; i++) {
-    SharedMemory<int> my_int("intTest3", O_RDWR, true, 0);
-    SharedMemory<int> your_int("intTest3", O_RDONLY, false, 0);
+    SharedMemory<int> my_int("intTest3", O_RDWR, true, false);
+    SharedMemory<int> your_int("intTest3", O_RDONLY, false, false);
     *my_int = i;
     EXPECT_EQ(*my_int, i);
     EXPECT_EQ(*my_int, *your_int);
@@ -48,13 +48,12 @@ TEST(SharedMemory, Remap) {
 }
 
 TEST(SharedMemory, Reattach) {
-  SharedMemory<int>* my_int = new SharedMemory<int>("intTest4", O_RDWR, true, 0);
-  // leak the memory to my_int so it's not destructed
-  pthread_t tid = pthread_self();
+  SharedMemory<int>* my_int = new SharedMemory<int>("intTest4", O_RDWR, true, false);
+  // leak memory so destructor isn't called
 
-  SharedMemory<int> re_int("intTest4", O_RDWR, true, tid);
+  SharedMemory<int> re_int("intTest4", O_RDWR, true, true);
   *re_int = 666;
 
-  EXPECT_EQ(*(*my_int), *re_int);
-
+  EXPECT_EQ(**my_int, *re_int);
 }
+
