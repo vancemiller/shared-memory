120ea5208da894d56d275800fab698f9b73cb822
Vance Miller <vance@cs.utexas.edu>
2020-10-25 14:57:02 +0000

    cleanup and move assignment

--- include/shared_memory.hpp
+++ include/shared_memory.hpp
@@ -7,107 +7,139 @@
 #include <fcntl.h>
 #include <iostream>
 #include <stdexcept>
-#include <system_error>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <system_error>
 #include <unistd.h>
 
 namespace wrapper {
 
 template <class T>
 class SharedMemory {
-  public:
-    const std::string name;
-  public:
-    const size_t nmemb;
-    const size_t size;
-  private:
+public:
+    std::string name;
+    size_t nmemb;
+
+private:
     int flags;
     FileDescriptor fd;
-  protected:
-    T* memory_region = NULL;
-  public:
+
+protected:
+    T *memory_region = NULL;
+
+public:
     /**
-      * Create a shared memory region of size nmemb * size identified by name.
-      * flags are POSIX shm (man shm_open) flags specifying permissions (O_RDONLY by default).
-      * Writable regions should set flags to O_RDWR.
-      * To create a new shared memory region, OR O_CREAT in to flags.
-      * To make sure a newly created memory region doesn't overwrite an existing one, OR O_EXCL
-      * in to flags.
-      */
-    SharedMemory(const std::string& name, size_t nmemb, size_t size, int flags = O_RDONLY) :
-        name(std::string("/") + name), nmemb(nmemb), size(size), flags(flags), fd([this] (void) ->
-            int {
-              if (sizeof(T) > this->size)
-                throw std::runtime_error("size must be at least " + std::to_string(sizeof(T)));
+     * @brief Create a shared memory region
+     * @param name the identifier the memory region
+     * @param nmemb the number of entries in the shared memory region (if it is
+     * an array)
+     * @param flags are POSIX shm (man shm_open) flags specifying permissions
+     * (O_RDONLY by default). Writable regions should set flags to O_RDWR. To
+     * create a new shared memory region, OR O_CREAT in to flags. To make sure a
+     * newly created memory region doesn't overwrite an existing one, OR O_EXCL
+     * in to flags.
+     */
+    SharedMemory(const char *name, size_t nmemb, int flags = O_RDONLY)
+        : name(std::string() + "/shared-memory-" + name), nmemb(nmemb),
+          flags(flags), fd([this](void) -> int {
               mode_t mode = (this->flags & O_CREAT) ? 0666 : 0;
               return shm_open(this->name.c_str(), this->flags, mode);
-            }()) {
-      if (fd == -1)
-        throw std::system_error(errno, std::generic_category(), "shm_open " + name + " failed");
-      if (flags & O_CREAT)
-        if (ftruncate(fd.get(), nmemb * size) == -1)
-          throw std::system_error(errno, std::generic_category(), "ftruncate failed");
-      int prot = PROT_READ;
-      if (flags & O_RDWR) prot |= PROT_WRITE;
-      if ((memory_region = (T*) mmap(NULL, nmemb * size, prot, MAP_SHARED, fd.get(), 0)) ==
-          MAP_FAILED)
-        throw std::system_error(errno, std::generic_category(), "mmap failed");
-   }
+          }()) {
+        if (fd == -1) {
+            throw std::system_error(errno, std::generic_category(),
+                                    std::string("shm_open ") + this->name +
+                                        " failed");
+        }
+        if (flags & O_CREAT) {
+            if (ftruncate(fd.get(), nmemb * sizeof(T)) == -1) {
+                throw std::system_error(errno, std::generic_category(),
+                                        "ftruncate failed");
+            }
+        }
+        int prot = PROT_READ;
+        if (flags & O_RDWR) {
+            prot |= PROT_WRITE;
+        }
+        if ((memory_region = (T *)mmap(NULL, nmemb * sizeof(T), prot,
+                                       MAP_SHARED, fd.get(), 0)) ==
+            MAP_FAILED) {
+            throw std::system_error(errno, std::generic_category(),
+                                    "mmap failed");
+        }
+    }
 
-    SharedMemory(const std::string& name, size_t size, int flags = O_RDONLY) :
-        SharedMemory(name, 1, size, flags) {}
-
-    ~SharedMemory(void) {
-      if (memory_region) {
-        if (munmap(memory_region, nmemb * size) == -1)
-          std::cerr << "munmap failed: " << std::strerror(errno) << std::endl;
-        if (flags & O_CREAT)
-          if (shm_unlink(name.c_str()) == -1)
-            std::cerr << "shm_unlink failed: " << std::strerror(errno) << std::endl;
-      }
+    SharedMemory(const char *name, int flags = O_RDONLY)
+        : SharedMemory(name, 1, flags) {}
+    SharedMemory() : nmemb(0), fd(-1) {}
+    ~SharedMemory() {
+        if (memory_region) {
+            if (munmap(memory_region, nmemb * sizeof(T)) == -1) {
+                std::cerr << "munmap failed: " << std::strerror(errno)
+                          << std::endl;
+            }
+            if (flags & O_CREAT) {
+                if (shm_unlink(name.c_str()) == -1) {
+                    std::cerr << "shm_unlink failed: " << std::strerror(errno)
+                              << std::endl;
+                }
+            }
+        }
     }
 
     // Delete copy constructors
-    SharedMemory(SharedMemory& o) = delete;
-    SharedMemory(const SharedMemory& o) = delete;
+    SharedMemory(SharedMemory &o) = delete;
+    SharedMemory(const SharedMemory &o) = delete;
     // Define move constructor
-    SharedMemory(SharedMemory&& o) : name(o.name), nmemb(o.nmemb), size(o.size), flags(o.flags),
-        fd(std::move(o.fd)), memory_region(o.memory_region) {
-      o.memory_region = NULL;
+    SharedMemory(SharedMemory &&o)
+        : name(o.name), nmemb(o.nmemb), flags(o.flags), fd(std::move(o.fd)),
+          memory_region(o.memory_region) {
+        o.memory_region = NULL;
+    }
+    SharedMemory &operator=(SharedMemory &&o) {
+        this->name = o.name;
+        this->nmemb = o.nmemb;
+        this->flags = o.flags;
+        this->fd = std::move(o.fd);
+        this->memory_region = o.memory_region;
+        o.memory_region = NULL;
+        return *this;
     }
 
-    const T& operator*(void) const {
-      if (!memory_region) throw std::runtime_error("nullptr");
-      return *memory_region;
+    const T &operator*(void) const {
+        if (!memory_region) {
+            throw std::runtime_error("nullptr");
+        }
+        return *memory_region;
     }
-    const T* operator&(void) const {
-      return memory_region;
+    const T *operator&(void) const { return memory_region; }
+    const T *operator->(void) const { return memory_region; }
+    const T &operator[](size_t i) const {
+        if (!memory_region) {
+            throw std::runtime_error("nullptr");
+        }
+        if (i > nmemb) {
+            throw std::out_of_range(std::to_string(i));
+        }
+        return ((T *)this->memory_region)[i];
     }
-    const T* operator->(void) const {
-      return memory_region;
+    T &operator*(void) {
+        if (!memory_region) {
+            throw std::runtime_error("nullptr");
+        }
+        return *memory_region;
     }
-    const T& operator[](size_t i) const {
-      if (!memory_region) throw std::runtime_error("nullptr");
-      if (i > nmemb) throw std::out_of_range(std::to_string(i));
-      return ((T*) this->memory_region)[i];
-    }
-    T& operator*(void) {
-      if (!memory_region) throw std::runtime_error("nullptr");
-      return *memory_region;
-    }
-    T* operator&(void) {
-      return memory_region;
-    }
-    T* operator->(void) {
-      return memory_region;
-    }
-    T& operator[](size_t i) {
-      if (!memory_region) throw std::runtime_error("nullptr");
-      if (i > nmemb) throw std::out_of_range(std::to_string(i));
-      return ((T*) this->memory_region)[i];
+    T *operator&(void) { return memory_region; }
+    T *operator->(void) { return memory_region; }
+    T &operator[](size_t i) {
+        if (!memory_region) {
+            throw std::runtime_error("nullptr");
+        }
+        if (i > nmemb) {
+            throw std::out_of_range(std::to_string(i));
+        }
+        return ((T *)this->memory_region)[i];
     }
 };
 
-} // namespace wrapper
+}  // namespace wrapper
 #endif
--- test/tests/shared_memory.cpp
+++ test/tests/shared_memory.cpp
@@ -1,149 +1,143 @@
-#include "gtest/gtest.h"
 #include "shared_memory.hpp"
+#include "gtest/gtest.h"
 
 #define ITERATIONS 6666
 namespace wrapper {
 
 TEST(SharedMemory, ConstructDestruct) {
-  SharedMemory<int> sharedInt("test", sizeof(int), O_RDWR | O_CREAT | O_EXCL);
+    SharedMemory<int> sharedInt("test", O_RDWR | O_CREAT | O_EXCL);
 }
 
 TEST(SharedMemory, ReadWrite) {
-  SharedMemory<int> my_int("test", sizeof(int), O_RDWR | O_CREAT | O_EXCL);
-  int i = 0;
-  for (*my_int = 0; *my_int < ITERATIONS; i++, (*my_int)++)
-    EXPECT_EQ(*my_int, i);
+    SharedMemory<int> my_int("test", O_RDWR | O_CREAT | O_EXCL);
+    int i = 0;
+    for (*my_int = 0; *my_int < ITERATIONS; i++, (*my_int)++)
+        EXPECT_EQ(*my_int, i);
 }
 
 TEST(SharedMemory, ReadOnly) {
-  SharedMemory<int> my_int("test", sizeof(int), O_RDWR | O_CREAT | O_EXCL);
-  SharedMemory<int> your_int("test", sizeof(int));
-  *my_int = 666;
-  EXPECT_EQ(*my_int, *your_int);
+    SharedMemory<int> my_int("test", O_RDWR | O_CREAT | O_EXCL);
+    SharedMemory<int> your_int("test");
+    *my_int = 666;
+    EXPECT_EQ(*my_int, *your_int);
 }
 
 TEST(SharedMemory, ReadWrite2) {
-  SharedMemory<int> my_int("test", sizeof(int), O_RDWR | O_CREAT | O_EXCL);
-  SharedMemory<int> our_int("test", sizeof(int), O_RDWR);
-  *my_int = 666;
-  EXPECT_EQ(*my_int, *our_int);
-  (*our_int)++;
-  EXPECT_EQ(*my_int, *our_int);
-  EXPECT_EQ(*my_int, 667);
+    SharedMemory<int> my_int("test", O_RDWR | O_CREAT | O_EXCL);
+    SharedMemory<int> our_int("test", O_RDWR);
+    *my_int = 666;
+    EXPECT_EQ(*my_int, *our_int);
+    (*our_int)++;
+    EXPECT_EQ(*my_int, *our_int);
+    EXPECT_EQ(*my_int, 667);
 }
 
 TEST(SharedMemory, Remap) {
-  for (int i = 0; i < ITERATIONS; i++) {
-    SharedMemory<int> my_int("test", sizeof(int), O_RDWR | O_CREAT | O_EXCL);
-    SharedMemory<int> your_int("test", sizeof(int), O_RDONLY);
-    *my_int = i;
-    EXPECT_EQ(*my_int, i);
-    EXPECT_EQ(*my_int, *your_int);
-  }
+    for (int i = 0; i < ITERATIONS; i++) {
+        SharedMemory<int> my_int("test", O_RDWR | O_CREAT | O_EXCL);
+        SharedMemory<int> your_int("test", O_RDONLY);
+        *my_int = i;
+        EXPECT_EQ(*my_int, i);
+        EXPECT_EQ(*my_int, *your_int);
+    }
 }
 
 TEST(SharedMemory, TransferOwnership) {
-  std::unique_ptr<SharedMemory<int>> my_int(std::make_unique<SharedMemory<int>>("test",
-      sizeof(int), O_RDWR | O_CREAT | O_EXCL));
-  **my_int = 666;
-  {
-    SharedMemory<int> your_int("test", sizeof(int));
-    my_int.reset();
-    EXPECT_EQ(666, *your_int);
-  }
-  EXPECT_THROW(SharedMemory<int> your_int("test", sizeof(int)), std::system_error);
+    std::unique_ptr<SharedMemory<int>> my_int(
+        std::make_unique<SharedMemory<int>>("test", O_RDWR | O_CREAT | O_EXCL));
+    **my_int = 666;
+    {
+        SharedMemory<int> your_int("test");
+        my_int.reset();
+        EXPECT_EQ(666, *your_int);
+    }
+  EXPECT_THROW(SharedMemory<int> your_int("test"), std::system_error);
 }
 
 const int nmemb = 8;
 
 TEST(SharedMemory, ConstructDestructArray) {
-  SharedMemory<int> sharedArray("test", nmemb, sizeof(int), O_RDWR | O_CREAT | O_EXCL);
+    SharedMemory<int> sharedArray("test", nmemb, O_RDWR | O_CREAT | O_EXCL);
 }
 
 TEST(SharedMemory, ReadWriteArray) {
-  SharedMemory<int> my_int("test", nmemb, sizeof(int), O_RDWR | O_CREAT | O_EXCL);
-  for (int i = 0; i < nmemb; i++) {
-    my_int[i] = i;
-    EXPECT_EQ(i, my_int[i]);
-  }
+    SharedMemory<int> my_int("test", nmemb, O_RDWR | O_CREAT | O_EXCL);
+    for (int i = 0; i < nmemb; i++) {
+        my_int[i] = i;
+        EXPECT_EQ(i, my_int[i]);
+    }
 }
 
 TEST(SharedMemory, ReadOnlyArray) {
-  SharedMemory<int> my_int("test", nmemb, sizeof(int), O_RDWR | O_CREAT | O_EXCL);
-  SharedMemory<int> your_int("test", nmemb, sizeof(int));
-  for (int i = 0; i < nmemb; i++) {
-    my_int[i] = i;
-    EXPECT_EQ(i, my_int[i]);
-  }
-  for (int i = 0; i < nmemb; i++) {
-    EXPECT_EQ(*my_int, *your_int);
-  }
+    SharedMemory<int> my_int("test", nmemb, O_RDWR | O_CREAT | O_EXCL);
+    SharedMemory<int> your_int("test", nmemb);
+    for (int i = 0; i < nmemb; i++) {
+        my_int[i] = i;
+        EXPECT_EQ(i, my_int[i]);
+    }
+    for (int i = 0; i < nmemb; i++) {
+        EXPECT_EQ(*my_int, *your_int);
+    }
 }
 
 TEST(SharedMemory, ReadWrite2Array) {
-  SharedMemory<int> my_int("test", nmemb, sizeof(int), O_RDWR | O_CREAT | O_EXCL);
-  SharedMemory<int> our_int("test", nmemb, sizeof(int), O_RDWR);
+    SharedMemory<int> my_int("test", nmemb, O_RDWR | O_CREAT | O_EXCL);
+    SharedMemory<int> our_int("test", nmemb, O_RDWR);
 
-  for (int i = 0; i < nmemb; i++) {
-    my_int[i] = i;
-  }
-  for (int i = 0; i < nmemb; i++) {
-    EXPECT_EQ(*my_int, *our_int);
-  }
-  for (int i = 0; i < nmemb; i++) {
-    our_int[i] = i * 2;
-  }
-  for (int i = 0; i < nmemb; i++) {
-    EXPECT_EQ(my_int[i], our_int[i]);
-  }
-}
-
-TEST(SharedMemory, NotBigEnough) {
-  EXPECT_THROW(new SharedMemory<int>("test", 1, (size_t) 1, O_RDWR | O_CREAT | O_EXCL),
-      std::runtime_error);
+    for (int i = 0; i < nmemb; i++) {
+        my_int[i] = i;
+    }
+    for (int i = 0; i < nmemb; i++) {
+        EXPECT_EQ(*my_int, *our_int);
+    }
+    for (int i = 0; i < nmemb; i++) {
+        our_int[i] = i * 2;
+    }
+    for (int i = 0; i < nmemb; i++) {
+        EXPECT_EQ(my_int[i], our_int[i]);
+    }
 }
 
 TEST(SharedMemory, MoveConstructor) {
-  SharedMemory<int> m("test", 1, sizeof(int), O_RDWR | O_CREAT | O_EXCL);
-  *m = 12345;
-  SharedMemory<int> m2(std::move(m));
-  EXPECT_EQ(12345, *m2);
-  EXPECT_THROW(*m, std::runtime_error);
+    SharedMemory<int> m("test", 1, O_RDWR | O_CREAT | O_EXCL);
+    *m = 12345;
+    SharedMemory<int> m2(std::move(m));
+    EXPECT_EQ(12345, *m2);
+    EXPECT_THROW(*m, std::runtime_error);
 }
 
 class DestructSetsInt {
-  int& to_set;
-  int value;
-  public:
-    DestructSetsInt(int& to_set, int value) : to_set(to_set), value(value) {}
+    int &to_set;
+    int value;
+
+public:
+    DestructSetsInt(int &to_set, int value) : to_set(to_set), value(value) {}
     ~DestructSetsInt(void) { to_set = value; }
 };
 
 TEST(SharedMemory, DestructDoesNotAffectContents) {
-  int i = 0;
-  {
-    SharedMemory<DestructSetsInt> shared("test", sizeof(DestructSetsInt), O_RDWR | O_CREAT |
-        O_EXCL);
-    new (&shared) DestructSetsInt(i, 2);
+    int i = 0;
+    {
+        SharedMemory<DestructSetsInt> shared("test", O_RDWR | O_CREAT | O_EXCL);
+        new (&shared) DestructSetsInt(i, 2);
+        EXPECT_EQ(0, i);
+    }
     EXPECT_EQ(0, i);
-  }
-  EXPECT_EQ(0, i);
 }
 
 TEST(SharedMemory, ExplicitDestructDoesDestruct) {
-  int i = 0;
-  {
-    SharedMemory<DestructSetsInt> shared("test", sizeof(DestructSetsInt), O_RDWR | O_CREAT |
-        O_EXCL);
-    new (&shared) DestructSetsInt(i, 2);
-    EXPECT_EQ(0, i);
-    shared->~DestructSetsInt();
-  }
-  EXPECT_EQ(2, i);
+    int i = 0;
+    {
+        SharedMemory<DestructSetsInt> shared("test", O_RDWR | O_CREAT | O_EXCL);
+        new (&shared) DestructSetsInt(i, 2);
+        EXPECT_EQ(0, i);
+        shared->~DestructSetsInt();
+    }
+    EXPECT_EQ(2, i);
 }
 
 TEST(SharedMemory, NotExist) {
-  EXPECT_THROW(SharedMemory<int> m("test", 1, sizeof(int), O_RDWR), std::runtime_error);
-  EXPECT_THROW(SharedMemory<int> m("test", 1, sizeof(int)), std::runtime_error);
+    EXPECT_THROW(SharedMemory<int> m("test", 1, O_RDWR), std::runtime_error);
+    EXPECT_THROW(SharedMemory<int> m("test", 1), std::runtime_error);
 }
-} // namespace wrapper
+}  // namespace wrapper
